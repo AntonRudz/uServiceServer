28ed1f213f8698a3104e5dbebbd2ccfafdfd6646
Anton <ukkonn8@gmail.com>
2019-06-14 13:28:17 +0300

    version 2

--- 2019-03-16_NikaionService/src/telran/security/WebSecurityConfig.java
+++ 2019-03-16_NikaionService/src/telran/security/WebSecurityConfig.java
@@ -1,27 +1,35 @@
 package telran.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-	 @Override
-	    protected void configure(HttpSecurity http) throws Exception {
-	        http.cors();
-	    }
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-	    @Bean
-	    CorsConfigurationSource corsConfigurationSource() {
-	        UrlBasedCorsConfigurationSource source = new
-	                UrlBasedCorsConfigurationSource();
-	        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-	        return source;
-	    }
-	
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebMvc
+public class WebSecurityConfig extends WebMvcConfigurerAdapter {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**").allowedOrigins("your url or just add * ").allowedMethods("PUT", "DELETE")
+				.allowedHeaders("header1", "header2", "header3").exposedHeaders("header1", "header2")
+				.allowCredentials(false).maxAge(3600);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("https://localhost:4200"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
 }
