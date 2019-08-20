89b0d7b57f1a8c2a942d472f7ba0c0d9fa6c8c0c
Anton <Anton@Comp>
2019-08-20 20:24:01 +0300

    new service

--- 2019-03-16_NikaionService/src/service/mongodb/repo/Schedulerepository.java
+++
@@ -1,9 +0,0 @@
-package telran.nikaion.mongodb.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import telran.nikaion.entities.mongodb.SchedulePatternCrud;
-
-public interface Schedulerepository extends MongoRepository<SchedulePatternCrud, String>{
-
-}
--- 2019-03-16_NikaionService/src/telran/security/dto/AccountsApi.java
+++
@@ -1,9 +0,0 @@
-package telran.security.dto;
-
-public interface AccountsApi {
-String ADD_ACCOUNT="/account/add";
-String REMOVE_ACCOUNT="/account/remove";
-String UPDATE_PASSWORD="/account/update";
-String USERNAME="username";
-String PASSWORD="password";
-}
